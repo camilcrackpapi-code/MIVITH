11d26cc59a1039d31904d40816e60b072f352cbf
SrMillan <camilcrackpapi@gmail.com>
2026-07-28 14:23:32 -0500

    Update firebase-config.js

--- controllers/firebase-config.js
+++ controllers/firebase-config.js
@@ -1,19 +1,17 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
-import { getAuth }       from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
-import { getFirestore }  from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
 
-// Sustituye estos valores con las credenciales obtenidas de tu Consola de Firebase
 const firebaseConfig = {
-  apiKey:            "TU_API_KEY",
-  authDomain:        "TU_PROJECT_ID.firebaseapp.com",
-  projectId:         "TU_PROJECT_ID",
-  storageBucket:     "TU_PROJECT_ID.appspot.com",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-  appId:             "TU_APP_ID"
+  apiKey: "AIzaSyDK2B1wJe2TpYvfyeQZM0i0sjeMNOR7H1A",
+  authDomain: "mivith.firebaseapp.com",
+  projectId: "mivith",
+  storageBucket: "mivith.firebasestorage.app",
+  messagingSenderId: "964463664776",
+  appId: "1:964463664776:web:91588eb4f604134a4fc93d"
 };
 
-const app  = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db   = getFirestore(app);
+// Inicializar Firebase (Corregido el cierre del paréntesis)
+const app = initializeApp(firebaseConfig);
 
-export { app, auth, db };
+// Exportar la autenticación para usarla en login.js
+export const auth = getAuth(app);
